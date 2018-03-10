e5191364758d0b0709655490cc614569f3b8b9c4
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-03-10 00:37:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var a = 5
+var b = 4 
+
+function add (a, b) {
+  return a + b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+
+
+function increment(n){
+ n++;
+  return n;
+}
